95fcf60c2fee9d404ecb87380fdf1041c2fc2ecf
Nneka Tielman <nneka.tielman@and.digital>
2023-07-18 15:52:09 +0200

    initial commit

---
+++ android/app/src/main/kotlin/com/example/daily_tarot/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.daily_tarot
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TarotApp());
+}
+
+class TarotApp extends StatelessWidget {
+  const TarotApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
+
+// TODO: decide on app layout
+// TODO: create basic app layout with card(s) in center
+// TODO: add static card "back"image
+// TODO: insert static card widget image(s) check if they load
+// TODO: add clicklistener to card(s)
+// TODO: add random package
+// TODO: handleclick function trigger random number to be chosen
+// TODO: change image along with randomnumber
+// change orientation??
+// TODO: style to look like tarot app
