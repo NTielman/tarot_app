13ed25fca95cc3065438edc038577a2207df791f
Nneka Tielman <nneka.tielman@and.digital>
2023-07-26 14:55:49 +0200

    fetch random card from api

--- lib/components/tarot_card.dart
+++ lib/components/tarot_card.dart
@@ -3,6 +3,10 @@ import 'card_back.dart';
 import 'card_front.dart';
 import 'dart:math';
 import 'package:audioplayers/audioplayers.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
+
+String apiUrl = 'https://tarot-api-3hv5.onrender.com/api/v1/cards/random?n=1';
 
 class TarotCard extends StatefulWidget {
   const TarotCard({super.key});
@@ -15,66 +19,33 @@ class _TarotCardState extends State<TarotCard> {
   String _cardName = '';
   bool _cardOrientation = true;
   bool _firstTap = true;
-  final List<String> _cardTypes = ['ar', 'cu', 'pe', 'sw', 'wa'];
-  final List<String> _arcanaValues = [
-    '00',
-    '01',
-    '02',
-    '03',
-    '04',
-    '05',
-    '06',
-    '07',
-    '08',
-    '09',
-    '10',
-    '11',
-    '12',
-    '13',
-    '14',
-    '15',
-    '16',
-    '17',
-    '18',
-    '19',
-    '20',
-    '21',
-  ];
-  final List<String> _cardValues = [
-    '02',
-    '03',
-    '04',
-    '05',
-    '06',
-    '07',
-    '08',
-    '09',
-    '10',
-    'ac',
-    'ki',
-    'kn',
-    'pa',
-    'qu'
-  ];
   final AudioPlayer player = AudioPlayer();
 
   void _handleTap() async {
     await player.play(AssetSource('card-flip.wav'));
+
+    try {
+      http.Response res = await http.get(Uri.parse(apiUrl));
+      if (res.statusCode == 200) {
+        var jsonResponse = await convert.jsonDecode(res.body);
+        var randCard = jsonResponse['cards'][0];
+
+        setState(() {
+          _cardName = '${randCard['name_short']}';
+          _cardOrientation = Random().nextBool();
+        });
+      } else {
+        throw ('Request failed with status: ${res.statusCode}');
+      }
+    } catch (error) {
+      print(error);
+    }
+
     if (_firstTap) {
       setState(() {
         _firstTap = false;
       });
     }
-
-    String suite = _cardTypes[Random().nextInt(_cardTypes.length)];
-    String value = suite == 'ar'
-        ? _arcanaValues[Random().nextInt(_arcanaValues.length)]
-        : _cardValues[Random().nextInt(_cardValues.length)];
-
-    setState(() {
-      _cardName = '$suite$value';
-      _cardOrientation = Random().nextBool();
-    });
   }
 
   @override
